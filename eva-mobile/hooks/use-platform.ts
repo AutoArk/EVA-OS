3123776650be337107e5dabdb649783d8d7d8140
chenbin11200 <chenbin11200@users.noreply.github.com>
2026-08-03 17:02:01 +0800

    EVA platform new release (#37)
    * update readme
    
    * delete SDK and examples, will be moved to example repo and sdk registry
    
    * 更新标题头

--- eva-mobile/hooks/use-platform.ts
+++
@@ -1,8 +0,0 @@
-import { Platform } from "react-native";
-
-
-export function usePlatform() {
-  const isIOS = Platform.OS === "ios";
-  const isAndroid = Platform.OS === "android";
-  return { isIOS, isAndroid };
-}
